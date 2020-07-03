54567c63ec5fca4d69b5673efedce756c25ec356
Gim kelum <49275770+gim96@users.noreply.github.com>
2020-07-03 21:10:28 +0530

    Updates

---
+++ precache-manifest.33938a4466d06dcbf1dd569739712bcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68d9ed5ad15254c8097da8b161a6a6e",
+    "url": "/test_js/index.html"
+  },
+  {
+    "revision": "f2b5fc520daffc299f37",
+    "url": "/test_js/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c0e4c54a447e08f7bbbb",
+    "url": "/test_js/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f2b5fc520daffc299f37",
+    "url": "/test_js/static/js/2.00872b52.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/test_js/static/js/2.00872b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e4c54a447e08f7bbbb",
+    "url": "/test_js/static/js/main.b95cb45f.chunk.js"
+  },
+  {
+    "revision": "af7752d352dbb68421b4",
+    "url": "/test_js/static/js/runtime-main.ea2193bb.js"
+  }
+]);
